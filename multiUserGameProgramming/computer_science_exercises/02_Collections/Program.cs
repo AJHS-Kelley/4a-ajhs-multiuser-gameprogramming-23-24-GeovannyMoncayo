7bc8a02225ccaa88aaf6ad02269c8860d25a4b5b
Geovanny <s11575131@students.duvalschools.org>
2023-10-03 14:22:28 -0400

    v0.1b --
    Print elements from array on single line.

--- multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_Collections, Geovanny Moncayo, 10-3-23, v0.0
+﻿// 02_Collections, Geovanny Moncayo, 10-3-23, v0.1b
 using System;
 
 namespace _02_Collections
@@ -22,6 +22,15 @@ namespace _02_Collections
             string[] breakfastFoods = { "Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
             int[] testScores = { 95, 100, 25, 15, 27, 35};
             float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
+            // Print Array Contents -- All Elements on Single Line
+            Console.WriteLine("The elements for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
+            Console.WriteLine();
         }
     }
 }
