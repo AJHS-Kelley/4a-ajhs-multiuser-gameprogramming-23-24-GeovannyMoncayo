08075d938d9efb1c722b22fe0aa460f8e10e022f
Geovanny <s11575131@students.duvalschools.org>
2023-10-24 14:09:19 -0400

    v0.1
    Made a method and called it.

---
+++ multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
@@ -0,0 +1,21 @@
+// Geovanny Moncayo, Methods and Parameters, v0.1
+
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {   
+        // METHOD -- name block of code, can be used over and over.
+        // All methods have a SIGNATURE that defines their name, parameters, and 
+        // Example Signature
+        static void MyMethod()
+        {
+            Console.WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and french fried potatoes.\n");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
