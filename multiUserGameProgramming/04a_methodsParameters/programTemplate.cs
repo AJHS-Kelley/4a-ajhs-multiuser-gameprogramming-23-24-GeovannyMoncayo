074eabdabbc091f9b0b04e34c2a390d24b8bfc5e
Geovanny <s11575131@students.duvalschools.org>
2023-10-24 14:28:10 -0400

    v0.2
    Added DoubleUp method with return statement.

--- multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
+++ multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
@@ -1,4 +1,4 @@
-// Geovanny Moncayo, Methods and Parameters, v0.1
+// Geovanny Moncayo, Methods and Parameters, v0.2
 
 using System;
 
@@ -7,15 +7,29 @@ namespace MethodsParameters
     class MethodsParameters
     {   
         // METHOD -- name block of code, can be used over and over.
-        // All methods have a SIGNATURE that defines their name, parameters, and 
+        // All methods have a SIGNATURE that defines their name, parameters, and ************************************
         // Example Signature
         static void MyMethod()
         {
             Console.WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and french fried potatoes.\n");
         }
+        // static -- This method belongs to the current class, it is NOT an object ************************************
+        // void -- This method has no return value.
+        
+        static int DoubleUp()
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enter a number on the next line.\n");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            //sum *= 2;
+            //Console.WriteLine(sum);
+            return sum *= 2;
+        }
         static void Main(string[] args)
         {
-            MyMethod();
+            //MyMethod();
+            DoubleUp();
         }
     }
 }
