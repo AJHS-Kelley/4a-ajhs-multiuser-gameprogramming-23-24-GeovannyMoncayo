38f3694b13909a59b4210ced5e7d1ec68a13cec3
Geovanny <77407144+GeovannyMoncayo@users.noreply.github.com>
2023-11-16 15:01:34 -0500

    jhg

---
+++ multiUserGameProgramming/05a_exampleGameMethods/programTemplate.cs
@@ -0,0 +1,35 @@
+// Geovanny Moncayo, Program Template, v0.0
+
+using System;
+
+namespace Operators
+{
+    class Program
+
+    {   
+        static int checkInventory (ArrayList Inventory = "empty")
+        {
+            var checkList = new ArrayList()
+            for (int i = 0; i < Inventory.length-1; i++)
+            {
+                
+            }
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            var myInventory = new ArrayList()
+            {
+                "Wood", "Stone", "Stone", "Water Bottle"
+            }
+            checkInventory(myInventory);
+            
+        }
+        
+    }
+}
