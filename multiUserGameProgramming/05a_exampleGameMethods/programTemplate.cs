010811b577fb46a339c5cd343e3170f538158581
Geovanny <s11575131@students.duvalschools.org>
2023-11-27 14:58:19 -0500

    afgsdf
    Made updates

--- multiUserGameProgramming/05a_exampleGameMethods/programTemplate.cs
+++ multiUserGameProgramming/05a_exampleGameMethods/programTemplate.cs
@@ -1,18 +1,53 @@
 // Geovanny Moncayo, Program Template, v0.0
 
 using System;
+using System.Collections;
 
 namespace Operators
 {
     class Program
 
     {   
-        static int checkInventory (ArrayList Inventory = "empty")
+        static void checkInventory (ArrayList Inventory)
         {
-            var checkList = new ArrayList()
-            for (int i = 0; i < Inventory.length-1; i++)
+            var checkList = new ArrayList();
+            //cycles through all items in Inventory
+            for (int i = 0; i < Inventory.Count; i++)
             {
-                
+                //If the checkList doesn't contain the item, for example "Stone", then it would skip it and go onto "Wood"
+                if (checkList.Contains(Inventory[i]) == false)
+                {
+                    // Adds "Stone" to checkList
+                    checkList.Add(Inventory[i]);
+                    // sets "Stone to object conItem
+                    object conItem = Inventory[i];
+                    Inventory.Remove(conItem);
+                    var amount = 1;
+                    foreach(var item in checkList)
+                    {
+                        Console.WriteLine("first " + item);
+                    }
+                    for (int x = 0; x < Inventory.Count; x++)
+                    {
+                        if(Inventory.Contains(conItem))
+                        {
+                            amount++;
+                            Inventory.Remove(x);
+                            Console.WriteLine("second " + conItem);
+                            Console.WriteLine("second " + amount);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    checkList.Add(amount);
+                }
+            }
+            //Console.WriteLine("You have:" + string.Join(":\n", checkList));
+            foreach(var item in checkList)
+            {
+                Console.WriteLine(item);
             }
         }
 
@@ -25,8 +60,8 @@ namespace Operators
         {
             var myInventory = new ArrayList()
             {
-                "Wood", "Stone", "Stone", "Water Bottle"
-            }
+                "Wood", "Stone", "Stone", "x", "x", "x", "x"
+            };
             checkInventory(myInventory);
             
         }
