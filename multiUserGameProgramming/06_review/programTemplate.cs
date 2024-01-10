3b6dfa2c5fd23828078fc8e8a47fc7324d573368
Geovanny <s11575131@students.duvalschools.org>
2024-01-10 14:05:17 -0500

    v0.0
    Review Project variables made

--- multiUserGameProgramming/06_review/programTemplate.cs
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -1,14 +1,43 @@
-// Geovanny Moncayo, Program Template, v0.0
+// Geovanny Moncayo, Review Project, v0.0
 
 using System;
 
-namespace Operators
+namespace ReviewProject
 {
-    class Program
+    class ReviewProject
     {
         static void Main(string[] args)
         {
-        
+            // Declaring and Assigning Variables
+            // Integer -- -2,147,483,648 to 2,147,483,647
+            // Integer = 4 Bytes.
+            int exaInt = 1;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            // Long = 8 Bytes
+            Long myLong = 9,223,372,036,854,775,000;
+
+            // Float -- Stores up to 6-7 decimal digits.
+            // Float = 4 Bytes
+            float exaFloat = 0.1f;
+
+            // Double -- Stores up to 15 decimal digits.
+            // Double = 8 Bytes
+            double myDouble = 17.12342134321243;
+
+            // Boolean -- true / false
+            // Boolean = 1 bit
+            bool exaBool = true;
+
+            // String
+            // 2-btes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+            string exaString = " ";
+
+            // Character (char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-bytes
+            char myChar = 'C';
         }
     }
 }
