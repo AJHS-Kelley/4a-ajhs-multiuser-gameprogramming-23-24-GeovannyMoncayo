5a75079e6380e1a4881672f54c4dc1c1dd98b143
Geovanny <s11575131@students.duvalschools.org>
2024-01-10 14:46:33 -0500

    v0.1
    used switch statement for dayOfWeek

--- multiUserGameProgramming/06_review/programTemplate.cs
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -1,4 +1,4 @@
-// Geovanny Moncayo, Review Project, v0.0
+// Geovanny Moncayo, Review Project, v0.1
 
 using System;
 
@@ -15,7 +15,7 @@ namespace ReviewProject
 
             // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
             // Long = 8 Bytes
-            Long myLong = 9,223,372,036,854,775,000;
+            long myLong = 9223372036854775000;
 
             // Float -- Stores up to 6-7 decimal digits.
             // Float = 4 Bytes
@@ -23,7 +23,7 @@ namespace ReviewProject
 
             // Double -- Stores up to 15 decimal digits.
             // Double = 8 Bytes
-            double myDouble = 17.12342134321243;
+            double exaDouble = 17.12342134321243D;
 
             // Boolean -- true / false
             // Boolean = 1 bit
@@ -38,6 +38,57 @@ namespace ReviewProject
             // Single character, in SINGLE QUOTES ' '
             // 2-bytes
             char myChar = 'C';
+
+            // Type Casting -- change from one data type to another.
+            // IMPLICIT -- happens automatically when going smaller -> larger
+            // char -> int -> long -> float -> double
+
+            int myInt = 9;
+            double myDouble = myInt;
+            Console.WriteLine(myDouble);
+
+            // EXPLICIT -- Must be done manually when going larger -> smaller
+            // double -> float -> long -> int -> char
+            double exampleDouble = 9.1234567891D;
+            int exampleInt = (int) exampleDouble;
+            Console.WriteLine(exampleInt);
+            
+            // Conversion Methods
+
+            string newString = Convert.ToString(myChar); // String
+            double newDouble = Convert.ToDouble(exaFloat); // Double
+            //int newInt = Convert.ToInt32(myLong); // Integer
+            //long newLong = Convert.ToInt64(exaInt); // Long
+            
+            // Switch Statement
+            int dayOfWeek = 9;
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("The day is Sunday.\n");
+                    break;
+                case 2:
+                    Console.WriteLine("The day is Monday.\n");
+                    break;
+                case 3:
+                    Console.WriteLine("The day is Tuesday.\n");
+                    break;
+                case 4:
+                    Console.WriteLine("The day is Wednesday.\n");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is Thursday.\n");
+                    break;
+                case 6:
+                    Console.WriteLine("The day is Friday.\n");
+                    break;
+                case 7:
+                    Console.WriteLine("The day is Saturday.\n");
+                    break;
+                default:
+                    Console.WriteLine("No match found.\n");
+                    break;
+            }
         }
     }
 }
